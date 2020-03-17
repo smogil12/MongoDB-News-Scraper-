9510887b560650f1d45cb274ae8c13d67ad483c7
Spencer Mogil <smogil12@gmail.com>
2020-03-17 13:41:35 -0400

    feat: articles now appened to main page. will revisit hw after react to stylize

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlinesdb2";
 
 mongoose.connect(MONGODB_URI);
 
